dd4411c9632a098e65377e25acab5286d9a468b9
presstek2258 <aramirez2258@gmail.com>
2025-03-27 22:40:22 -0400

    show favourite methods added (not tested yet)

--- src/main/java/UserDatabase.java
+++ src/main/java/UserDatabase.java
@@ -1,3 +1,7 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,11 +11,65 @@ public class UserDatabase {
 
     //////////////////// "GREEN"  /////////////////
 
-    private Map<String, List<Integer>> userFavourites = new HashMap<>();
+    private final String jsonFileName = "favourites.json";
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    // reads a hashmap from the json file
+    public Map<String, List<Integer>> jsonToHashmap(){
+        try{
+            // make sure the file exists
+            File file = new File(jsonFileName);
+            if (!file.exists()){
+                return new HashMap<>();
+            }
+
+            // read and parse the json
+            return objectMapper.readValue(
+                    file,
+                    new TypeReference<Map<String, List<Integer>>>() {}
+            );
+
+        }catch (Exception error){
+            error.printStackTrace();
+            System.out.println("Error while reading json (jsonToHashmap)");
+        }
+        return null;
+    }
+
+    // writes the hashmap to the json file
+    public void hashmapToJson(Map<String, List<Integer>> userFavourites){
+        try{
+            // write the json file
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(jsonFileName), userFavourites);
+
+        }catch (Exception error){
+            error.printStackTrace();
+            System.out.println("Error while writing json (hashmapToJson)");
+        }
+    }
 
     public void addFavourite(String user, int itemID) {
 //        // If the user doesn't exist in the map, create a new list for their favourites
 //        userFavourites.computeIfAbsent(user, k -> new ArrayList<>()).add(itemID);
+
+        try {
+            // first fetch from the json
+            Map<String, List<Integer>> userFavourites = jsonToHashmap();
+
+            // add if the item to the array
+            if (!userFavourites.containsKey(user)) {
+                userFavourites.put(user, new ArrayList<>());
+            }else{
+                userFavourites.get(user).add(itemID);
+            }
+
+            // write back to the json
+            hashmapToJson(userFavourites);
+
+        }catch(Exception error) {
+            error.printStackTrace();
+            System.out.println("Error while adding favourite item (addFavourite)");
+        }
     }
 
     public int[] getFavourites(String user) {
@@ -19,7 +77,20 @@ public class UserDatabase {
 //        List<Integer> favourites = userFavourites.getOrDefault(user, new ArrayList<>());
 //        // Convert the list of integers to an array of primitives
 //        return favourites.stream().mapToInt(i -> i).toArray();
-        return new int[]{};
+
+        try {
+            // fetch the hashmap from json file
+            Map<String, List<Integer>> userFavourites = jsonToHashmap();
+
+            // get the favourites and return them as an int[]
+            List<Integer> favourites = userFavourites.getOrDefault(user, new ArrayList<>());
+            return favourites.stream().mapToInt(i -> i).toArray();
+
+        }catch(Exception error) {
+            error.printStackTrace();
+            System.out.println("Error while getting favourite items (getFavourites)");
+        }
+        return null;
     }
 
     public void removeFavourite(String user, int itemID) {
@@ -27,6 +98,22 @@ public class UserDatabase {
 //        if (userFavourites.containsKey(user)) {
 //            userFavourites.get(user).remove(Integer.valueOf(itemID));
 //        }
+        try {
+            // fetch the favourites from the json
+            Map<String, List<Integer>> userFavourites = jsonToHashmap();
+
+            // remove the user if it exists
+            if (userFavourites.containsKey(user)) {
+                userFavourites.get(user).remove(itemID);
+            }
+
+            // write back to the json
+            hashmapToJson(userFavourites);
+
+        }catch (Exception error) {
+            error.printStackTrace();
+            System.out.println("Error while removing favourite item (removeFavourite)");
+        }
     }
 
     //////////////////// "REFACTOR"  /////////////////
